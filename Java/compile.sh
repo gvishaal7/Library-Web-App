dc01a069b0a1e5ec8e76b4eb29b988f0758ad344
Vishaal Gopalan <gvishaal7@users.noreply.github.com>
2017-01-18 13:56:24 -0800

    Add files via upload

---
+++ Java/compile.sh
@@ -0,0 +1,16 @@
+HOME_DIR=/Users/vishaalgopalan/Desktop/Softwares/apache-tomcat-8.5.9/lib
+
+export CLASSPATH=$HOME_DIR/gson-2.6.1.jar:$HOME_DIR/servlet-api.jar
+
+JDK_HOME=/usr/bin
+
+$JDK_HOME/javac -d jar validate.java
+$JDK_HOME/javac -d jar adminOperations.java
+$JDK_HOME/javac -d jar userOperations.java 
+
+cd jar
+$JDK_HOME/jar -cvf com.jar com
+cp com.jar ../../WEB-INF/lib
+cp -rf com.jar ../../../../lib
+cd ..
+
